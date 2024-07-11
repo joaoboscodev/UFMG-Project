f681daa2de877ae3aa5738dc9cede1ed46566230
João Bosco <joaopanzera@hotmail.com>
2024-07-11 08:55:06 -0300

    Feat: cabeçalho criado

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,12 +1,22 @@
-// src/Dashboard.js
-
 import React from 'react';
+import '../Styles/News.css';
+import logo from '../images/brasao.png';
 
 function News() {
   return (
     <div>
+      <Header />
       <h2>News</h2>
       <p>Welcome to the News page!</p>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="title">UFMG - Search Project</h1>
     </div>
   );
 }
