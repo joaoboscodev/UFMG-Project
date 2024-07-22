ef08da49515887a4b6ea4c4782c0a414f79b2507
kenpatsu <joaommrocha23@gmail.com>
2024-07-22 18:10:39 -0300

    Scrapper atualizado 2207

--- server/index.js
+++ server/index.js
@@ -129,40 +129,47 @@ app.get('/api/search', async (req, res) => {
 //   }
 // });
 
-// app.get('/api/search', async (req, res) => {
-//   const { keyword, iniciodia, iniciomes, inicioano, fimdia, fimmes, fimano } = req.query;
+app.get('/api/search', async (req, res) => {
+  const { keyword, iniciodia, iniciomes, inicioano, fimdia, fimmes, fimano } = req.query;
   
-//   let url = `https://cse.google.com/cse?oe=utf8&ie=utf8&source=uds&q=${keyword}`;
-//   if (iniciodia && iniciomes && inicioano && fimdia && fimmes && fimano) {
-//     url += `+after:${inicioano}-${iniciomes}-${iniciodia}+before:${fimano}-${fimmes}-${fimdia}&lr=&safe=off&filter=0&gl=&cr=&as_sitesearch=*.uol.com.br/*&as_oq=&cx=33c20c29942ff412b&start=0#gsc.tab=0&gsc.q=biden after%3A2024-07-15 before%3A2024-07-20&gsc.page=1`;
-//     console.log(url);
-//   }
+  let url = `https://cse.google.com/cse?oe=utf8&ie=utf8&source=uds&q=${keyword}`;
+  if (iniciodia && iniciomes && inicioano && fimdia && fimmes && fimano) {
+    url += `+after:${inicioano}-${iniciomes}-${iniciodia}+before:${fimano}-${fimmes}-${fimdia}&lr=&safe=off&filter=0&gl=&cr=&as_sitesearch=*.uol.com.br/*&as_oq=&cx=33c20c29942ff412b&start=0#gsc.tab=0&gsc.q=biden after%3A2024-07-15 before%3A2024-07-20&gsc.page=1`;
+    console.log(url);
+  }
 
-//   try {
-//     const response = await axios.get(url);
-//     if (response.status === 200) {
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-//       console.log(html);
-//       const results = [];
+  
+  const rendertronUrl = `https://render-tron.appspot.com/render/${encodeURIComponent(url)}`;
 
-//       $('div.gsc-webResult.gsc-result').each((i, element) => {
-//         const link = $(element).find('a.gs-title').attr('href');
-//         const title = $(element).find('a.gs-title').text().trim();
-//         const image = $(element).find('img.gs-image').attr('src');
-//         console.log(title);
+  try {
+    const response = await axios.get(rendertronUrl, {
+      headers: {
+        'User-Agent': 'Rendertron',
+      },
+    });
 
-//         results.push({ link, title, image });
-//       });
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const results = [];
 
-//       res.json(results);
-//     } else {
-//       res.status(response.status).send(`Request failed with status code: ${response.status}`);
-//     }
-//   } catch (error) {
-//     res.status(error.response ? error.response.status : 500).send(`Request failed with status code: ${error.response ? error.response.status : 'unknown'}`);
-//   }
-// });
+      $('div.gsc-webResult.gsc-result').each((i, element) => {
+        const link = $(element).find('a.gs-title').attr('href');
+        const title = $(element).find('a.gs-title').text().trim();
+        const image = $(element).find('img.gs-image').attr('src');
+        console.log(title);
+
+        results.push({ link, title, image });
+      });
+
+      res.json(results);
+    } else {
+      res.status(response.status).send(`Request failed with status code: ${response.status}`);
+    }
+  } catch (error) {
+    res.status(error.response ? error.response.status : 500).send(`Request failed with status code: ${error.response ? error.response.status : 'unknown'}`);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
